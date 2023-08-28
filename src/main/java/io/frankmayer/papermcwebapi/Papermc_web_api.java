a677c88c0d27d519602c43871bc084ac75b0584b
Frank Mayer <git@frank-mayer.io>
2023-08-28 16:36:43 +0200

    initial commit

---
+++ src/main/java/io/frankmayer/papermcwebapi/Papermc_web_api.java
@@ -0,0 +1,17 @@
+package io.frankmayer.papermcwebapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Papermc_web_api extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
